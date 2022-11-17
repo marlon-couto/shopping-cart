8c1272509a3aeb0818f4beddaea5630f5c8c7d52
Marlon Couto <marloncouto199@gmail.com>
2022-11-17 02:25:17 -0300

    Refatora funções para melhorar sua legibilidade
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,13 +5,15 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+function createProduct(product) {
+  const sectionProducts = document.querySelector('section.products');
+  const newElement = createProductElement(product);
+  sectionProducts.appendChild(newElement);
+}
+
 async function populateSectionProducts() {
   const products = await fetchProductsList('computador');
-  products.forEach((product) => {
-    const sectionProducts = document.querySelector('section.products');
-    const newElement = createProductElement(product);
-    sectionProducts.appendChild(newElement);
-  });
+  products.forEach((product) => createProduct(product));
 }
 
 populateSectionProducts();
