758933377d6dfc1b123bf164ff6b6dda2544ab25
Marlon Couto <marloncouto199@gmail.com>
2022-11-17 03:37:36 -0300

    Adiciona texto de carregamento antes de exibir os produtos na página
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@ import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
+const sectionProducts = document.querySelector('section.products');
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+function createLoadingText() {
+  const span = document.createElement('span');
+  sectionProducts.appendChild(span);
+  const loadingSpan = sectionProducts.lastChild;
+  loadingSpan.textContent = 'carregando...';
+  loadingSpan.classList.add('loading');
+}
+
+const removeLoadingText = () => document.querySelector('.loading').remove();
+
 function createProduct(product) {
-  const sectionProducts = document.querySelector('section.products');
   const newElement = createProductElement(product);
   sectionProducts.appendChild(newElement);
 }
@@ -14,6 +25,11 @@ function createProduct(product) {
 async function populateSectionProducts() {
   const products = await fetchProductsList('computador');
   products.forEach((product) => createProduct(product));
+  removeLoadingText();
 }
+
+if (sectionProducts.children.length < 1) {
+  createLoadingText();
+} 
 
 populateSectionProducts();
