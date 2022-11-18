3f9b1ae6df316a69e455e323d995be2a85cccf83
Marlon Couto <marloncouto199@gmail.com>
2022-11-18 10:21:32 -0300

    Adiciona funcionalidade de restaurar o carrinho de compras do local storage
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
+import { getSavedCartIDs, saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
-import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { createCartProductElement, createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
@@ -10,48 +10,54 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const sectionProducts = document.querySelector('section.products');
 
 // Mostra mensagens de aviso na página
-function displayLoadingText() {
+function createSpan() {
   const span = document.createElement('span');
   sectionProducts.appendChild(span);
+}
+
+function displayLoadingText() {
+  createSpan();
 
   const loadingSpan = sectionProducts.lastChild;
+
   loadingSpan.textContent = 'carregando...';
   loadingSpan.classList.add('loading');
 }
 
-const hideLoadingText = () => document.querySelector('.loading').remove();
-
 function displayErrorText(errorMessage) {
-  const span = document.createElement('span');
-  sectionProducts.appendChild(span);
+  createSpan();
 
   const errorSpan = sectionProducts.lastChild;
+
   errorSpan.textContent = errorMessage;
   errorSpan.classList.add('error');
 }
 
-// Adiciona produtos ao carrinho de compras
-async function addProductToCart(id) {
-  const productData = await fetchProduct(id);
-  const newProduct = createCartProductElement(productData);
+const hideLoadingText = () => document.querySelector('.loading').remove();
 
+// Adiciona produtos ao carrinho de compras
+async function addProductToCart(product) {
+  const newProduct = createCartProductElement(product);
   const cartProducts = document.querySelector('.cart__products');
+
   cartProducts.appendChild(newProduct);
 }
 
-// Popula a página com os produtos
+// Adiciona produtos à página
 function createProduct(product) {
   const newElement = createProductElement(product);
   sectionProducts.appendChild(newElement);
 
   const buttonProductAdd = sectionProducts.lastChild.lastChild;
 
-  buttonProductAdd.addEventListener('click', () => {
+  buttonProductAdd.addEventListener('click', async () => {
+    const productData = await fetchProduct(product.id);
+    addProductToCart(productData);
     saveCartID(product.id);
-    addProductToCart(product.id);
   });
 }
 
+// Popula a página com os produtos
 async function getProductsList(searchTerm) {
   const products = await fetchProductsList(searchTerm)
     .catch(() => {
@@ -66,9 +72,26 @@ async function populateSectionProducts(searchTerm) {
   hideLoadingText();
 }
 
+// Restaura o Local Storage
+function restoreCart() {
+  const savedProducts = getSavedCartIDs();
+
+  const promises = savedProducts.map((savedProduct) => fetchProduct(savedProduct));
+
+  Promise.all(promises).then((values) => {
+    values.forEach((value) => {
+      addProductToCart(value);
+    });
+  });
+}
+
 // Chama as funções
 if (sectionProducts.children.length < 1) {
   displayLoadingText();
 }
 
 populateSectionProducts('computador');
+
+if (localStorage.getItem('cartProducts')) {
+  restoreCart();
+}
