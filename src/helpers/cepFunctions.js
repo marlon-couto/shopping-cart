552a3c04520ded619a18fb84549caba23ef4281d
Marlon Couto <marloncouto199@gmail.com>
2022-11-18 19:20:00 -0300

    Adiciona feature para exibir o endereço de entrega ao digitar o CEP no campo de busca
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,23 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (postalCode) => {
+  const url1 = `https://cep.awesomeapi.com.br/json/${postalCode}`;
+  const url2 = `https://brasilapi.com.br/api/cep/v2/${postalCode}`;
+
+  const response = await Promise.any([fetch(url1), fetch(url2)]);
+  const data = await response.json();
+
+  return data;
 };
 
 export const searchCep = () => {
-  // seu código aqui
+  const postalCode = document.querySelector('.cep-input').value;
+  const cartAdress = document.querySelector('.cart__address');
+
+  getAddress(postalCode)
+    .then((data) => {
+      const { address, district, city, state } = data;
+      cartAdress.textContent = `${address} - ${district} - ${city} - ${state}`;
+    })
+    .catch(() => {
+      cartAdress.textContent = 'CEP não encontrado';
+    });
 };
